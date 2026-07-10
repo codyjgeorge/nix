92814c244413940985e2b80f5a48b6af02a8412d
codyjgeorge <codygeorge315@gmail.com>
2026-07-10 12:43:36 +0200

    changed zsh prompt theme to philips

--- hm/zsh.nix
+++ hm/zsh.nix
@@ -33,7 +33,7 @@
                 plugins = [
                     "git"
                 ];
-                theme = "robbyrussell";
+                theme = "philips";
             };
         };
 }
