4f3bc96e25b34a306571a5ff667ecf395e1da643
codyjgeorge <codygeorge315@gmail.com>
2026-07-12 00:07:32 +0200

    added some nvf config

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -4,14 +4,64 @@
         programs.nvf = {
                 enable = true;
                 settings = {
-                        vim.viAlias = false;
-                        vim.vimAlias = true;
-                        vim.lsp = {
-                                enable = true;
-                        };
-                        vim.clipboard = {
-                                enable = true;
-                                registers = "unnamedplus";
+                        vim = {
+                                startPlugins = [
+                                        "snacks-nvim"
+                                        "noice-nvim"
+                                        "copilot-lua"
+                                        "telescope"
+                                        "smart-splits"
+                                        "render-markdown-nvim"
+                                        "precognition-nvim"
+                                        "nvim-lint"
+                                        "nvim-colorizer-lua"
+                                        "dracula"
+                                ];
+                                viAlias = false;
+                                vimAlias = true;
+                                lsp = {
+                                        enable = true;
+                                };
+                                languages = {
+                                        markdown = {
+                                                enable = true;
+                                                extensions.render-markdown-nvim = true;
+                                                format.enable = true;
+                                        };
+                                        nix = {
+                                                enable = true;
+                                                format.enable = true;
+                                        };
+                                };
+                                clipboard = {
+                                        enable = true;
+                                        registers = "unnamedplus";
+                                };
+                                theme = {
+                                        enable = true;
+                                        transparent = true;
+                                        name = "dracula"; # "base16" for custom. requires all vim.theme.base16-colors to be set.
+                                };
+                                options = {
+                                        wrap = false;
+                                        mouse = "a";
+                                        splitright = true;
+                                        tabstop = 4;
+                                        autoindent = true;
+                                        shiftwidth = 4;
+                                };
+
+                                ui = {
+                                        noice = {
+                                                enable = true;
+                                        };
+                                        colorizer = {
+                                                enable = true;
+                                        };
+                                };
+                                telescope = {
+                                        enable = true;
+                                };
                         };
                 };
         };
