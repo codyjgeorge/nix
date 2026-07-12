8c5358fa2f6529f923bb805ea66b1a730d947fb7
codyjgeorge <codygeorge315@gmail.com>
2026-07-12 15:42:37 +0200

    fixed hyprland/noctalia layer rule error

--- hm/hyprland.nix
+++ hm/hyprland.nix
@@ -386,11 +386,11 @@
                 name = "noctalia",
                 match = {
                     namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd)$",
-                    no_anim = true,
-                    ignore_alpha = 0.5,
-                    blur = true,
-                    blur_popups = true,
                 };
+                no_anim = true,
+                ignore_alpha = 0.5,
+                blur = true,
+                blur_popups = true,
             })
 		'';
 	};
