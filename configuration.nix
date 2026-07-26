93b71c700bd74ce691426631c017550bacd571aa
codyjgeorge <codygeorge315@gmail.com>
2026-07-26 22:58:11 +0200

    enabled pipewire and wireplumber for screensharing with xdg-portal

--- configuration.nix
+++ configuration.nix
@@ -94,7 +94,7 @@
     enable = true;
     extraPortals = [
         pkgs.xdg-desktop-portal-hyprland
-        pkgs.xdg-dekstop-portal-gtk
+        pkgs.xdg-desktop-portal-gtk
     ];
     config = {
         common.default = [ "gtk" ];
