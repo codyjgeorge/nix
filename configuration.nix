f2fb755b16783d24fab6f6b1b5df10347f9d1d5e
codyjgeorge <codygeorge315@gmail.com>
2026-07-26 23:35:31 +0200

    installed gcc

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    gcc
     vim
     wget
     git
