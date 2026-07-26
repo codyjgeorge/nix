566080b2769c940cd31a74eb878820902321ebb9
codyjgeorge <codygeorge315@gmail.com>
2026-07-26 20:41:01 +0200

    nvf disabled base16 bg

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -71,6 +71,21 @@
                                                 base0F = "#f265b5"; #Delimiters/Special Tags
                                         };
                                 };
+                                extraLuaConfig = ''
+                                    local function clear_bg()
+                                        vim.api.nvim_set_hl(0, "Normal", { bg = "none"} )
+                                        vim.api.nvim_set_hl(0, "NormalNC", { bg = "none"} )
+                                        vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+                                        vim.api.nvim_set_hl(0, "SignColumn", { bg = "none"})
+                                    end
+
+                                    --Run whenever colorsceme changes
+                                    clear_bg()
+                                    vim.api.nvim_create_autocmd("ColorScheme", {
+                                        pattern = "*",
+                                        callback = clear_bg,
+                                    })
+                                '';
                                 options = {
                                         wrap = false;
                                         mouse = "a";
