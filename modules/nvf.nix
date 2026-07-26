6c5c270c4c90ddb08e7b40c3920ca31d8aee70a4
codyjgeorge <codygeorge315@gmail.com>
2026-07-26 20:47:19 +0200

    nvf disabled base16 bg

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -71,7 +71,7 @@
                                                 base0F = "#f265b5"; #Delimiters/Special Tags
                                         };
                                 };
-                                luaConfigRC = ''
+                                luaConfigPost = ''
                                     local function clear_bg()
                                         vim.api.nvim_set_hl(0, "Normal", { bg = "none"} )
                                         vim.api.nvim_set_hl(0, "NormalNC", { bg = "none"} )
