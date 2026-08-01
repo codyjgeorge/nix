0410df33e1fb171c2c03ff00c19d039475f22f80
codyjgeorge <codygeorge315@gmail.com>
2026-08-01 12:46:38 +0200

    added noctalia, obsidian, spicetify to exclusion list in /hm/stylix.nix

--- hm/stylix.nix
+++ hm/stylix.nix
@@ -6,6 +6,9 @@
             kitty.enable = false;
             hyprland.enable = false;
             nvf.enable = false;
+            obsidian.enable = false;
+            spicetify.enable = false;
+            noctalia.enable = false;
         };
     };
 }
