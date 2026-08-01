352a20b2aaa9be7655dd83ae32c480b44da52c63
codyjgeorge <codygeorge315@gmail.com>
2026-08-01 12:33:00 +0200

    installed stylix, excluded kitty, nvf, hyprland

---
+++ modules/stylix.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }:
+
+{
+    stylix = {
+        enable = true;
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/eldritch.yaml";
+        # image = ../wallpapers/wallpaper.png;
+
+        fonts = {
+            monospace = {
+                package = pkgs.nerd-fonts.jetbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+            sansSerif = {
+                package = pkgs.nerd-fonts.jerbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+            serif = {
+                package = pkgs.nerd-fonts.jerbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+        };
+
+        targets = {
+            kitty.enable = false;
+            hyprland.enable = false;
+            nvf.enable = false;
+        };
+    };
+}
