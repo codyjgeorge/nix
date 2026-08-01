13d6bd3e8c7ce2987f7feef5ed4725304cc626b2
codyjgeorge <codygeorge315@gmail.com>
2026-08-01 15:00:41 +0200

    removed neonvoid spictify fix

--- modules/overlays.nix
+++ modules/overlays.nix
@@ -3,20 +3,9 @@
 {
       nixpkgs = {
         overlays = [
-          (final: prev: {
-            # Temporary: test spicetify-cli wrapper fix from neonvoidx/nixpkgs
-            spicetify-cli = final.callPackage (
-              final.fetchFromGitHub {
-                owner = "neonvoidx";
-                repo = "nixpkgs";
-                rev = "spicetify-cli-fix";
-                hash = "sha256-EbHtnei7167fm5ZzK3LoRdjKQhgkMpLqF9ERH04phQU=";
-                name = "source";
-              } + "/pkgs/by-name/sp/spicetify-cli/package.nix"
-            ) { };
-          })
 
           inputs.nh.overlays.default
+
         ];
       };
 
