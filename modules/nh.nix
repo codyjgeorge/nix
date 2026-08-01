9dacf6f031051b8210e71d9789d75554e286dd64
codyjgeorge <codygeorge315@gmail.com>
2026-08-01 11:41:09 +0200

    enabled clean in nh

--- modules/nh.nix
+++ modules/nh.nix
@@ -3,5 +3,8 @@
 {
     programs.nh = {
         enable = true;
+        clean.enable = true;
+        clean.extraArgs = "--keep-since 4d --keep 3";
+        flake = "/home/cody/nix"; # sets NH_OS_FLAKE variable for you
     };
 }
