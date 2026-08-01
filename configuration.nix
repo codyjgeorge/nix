e134e320cb03dc86e3e1a9ffbbaa0df4b6f401ce
codyjgeorge <codygeorge315@gmail.com>
2026-08-01 14:39:56 +0200

    moved machine specific lines in configuration.nix to /hosts/nuc-nixos/default.nix

--- configuration.nix
+++ configuration.nix
@@ -6,15 +6,6 @@
 }:
 
 {
-  boot.loader = {
-    systemd-boot = {
-      enable = true;
-      configurationLimit = 10;
-    };
-
-    efi.canTouchEfiVariables = true;
-  };
-
   nix = {
 
     settings = {
@@ -76,8 +67,6 @@
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
 
-  time.timeZone = "Europe/Warsaw";
-
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
@@ -92,12 +81,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
-  boot.kernelParams = [
-    "i915.enable_psr=0"
-    "i915.enable_fbc=0"
-    "i915.enable_dc=0"
-  ];
 
   services.getty.autologinUser = "cody";
 
@@ -174,24 +157,5 @@
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "26.05"; # Did you read the comment?
 
 }
--- hosts/nuc-nixos/default.nix
+++ hosts/nuc-nixos/default.nix
@@ -3,4 +3,21 @@
 {
     # Per-host settings for nuc-nixos
     # networking.hostName is set automatically from the flake attribute name.
+    
+    boot.loader = {
+        systemd-boot = {
+            enable = true;
+            configurationLimit = 10;
+        };
+        efi.canTouchEfiVariables = true;
+    };
+    boot.kernelParams = [
+        "i915.enable_psr=0"
+        "i915.enable_fbc=0"
+        "i915.enable_dc=0"
+    ];
+    
+    system.stateVersion = "26.05"; # NEVER CHANGE THIS VERSION NUMBER
+
+    time.timeZone = "Europe/Warsaw";
 }
