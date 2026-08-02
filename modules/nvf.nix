a2795d2e291c6571ce293853913cbed140209416
codyjgeorge <codygeorge315@gmail.com>
2026-08-02 18:00:59 +0200

    configured snacks dashboard in nvf.nix

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -67,7 +67,7 @@ in
                                 utility."snacks-nvim" = {
                                     enable = true;
                                     setupOpts.dashboard = {
-                                        width = 60;
+                                        width = 36;
                                         pane_gap = 4;
                                         sections = [
                                             { section = "header"; }
